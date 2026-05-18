2899fcf00c85f2cd21d5e4564ae8241842b74f54
0180001-sketch <20241ctb0180001@estudantes.ifpr.edu.br>
2026-05-18 11:35:54 -0300

    random video working

--- Cena terror-Misterio/Assets/Scripts/RandomVideo.cs
+++ Cena terror-Misterio/Assets/Scripts/RandomVideo.cs
@@ -6,19 +6,31 @@ using UnityEngine.Video;
 public class RandomVideo : MonoBehaviour
 {
     [SerializeField] VideoPlayer videoPlayer;
-    float minInt = 1f;
-    float maxInt = 12f;
+    public string glitchVids = "videos";
+    [SerializeField]float minInt;
+    [SerializeField]float maxInt;
     void Start(){
         StartCoroutine(Randomizeplay());
     }
 
     IEnumerator Randomizeplay(){
         while(true){
-            videoPlayer.Play();
             float wait= Random.Range(minInt, maxInt);
+            PlayRandomVideo();
+            videoPlayer.Play();
             yield return new WaitForSeconds(wait);
             videoPlayer.Stop();
             yield return new WaitForSeconds(wait);
-            }
+        }
+    }
+
+     public void PlayRandomVideo()
+    {
+        VideoClip[] videos = Resources.LoadAll<VideoClip>(glitchVids);
+        if (videos.Length > 0){
+            int randomIndex = Random.Range(0, videos.Length);
+            videoPlayer.clip = videos[randomIndex];
+            //videoPlayer.Play();
+        }
     }
 }
